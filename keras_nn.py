9fbb61ecf78c7c8864a062bd7dac5e8a09fe4ed7
ethancura <68085222+ethancura@users.noreply.github.com>
2020-07-28 11:45:46 -0500

    Add files via upload

---
+++ keras_nn.py
@@ -0,0 +1,46 @@
+import keras
+import numpy as np
+from numpy import loadtxt
+from keras.models import Sequential
+from keras.layers import Dense
+#loading the data set
+dataset = loadtxt('OHEEPD.csv', delimiter=',')
+#splitting hte datat into x and y vars
+x=dataset[:,0:22]
+y=dataset[:,22:27]
+#defieing keras model
+model = Sequential()
+model.add(Dense(64, input_dim=22, activation='relu'))#creating model that expects rows of data with 19 vars and this is 1st hidden layer has 12 nodes uses relu activaiton funciton
+model.add(Dense(32,activation ='relu'))#2nd hidden layer has 8 nodes and relu activaiton function
+model.add(Dense(32,activation ='relu'))#3nd hidden layer has 8 nodes and relu activaiton function
+model.add(Dense(16,activation ='relu'))#4th hidden layer has 8 nodes and relu activaiton function
+model.add(Dense(5, activation='sigmoid'))# output layer has 1 node uses sigmoid activaiton funciton
+
+#Complie the Keras model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+#fit the model 
+#epoch-one pass through of all of the rows in a training set(one epoch is made of 1 or more batches)
+#batch-one or more samples considered by the model within an epoch before weights are updated
+model.fit(x,y, epochs=1, batch_size=32)
+
+#evaluate the model
+_, accuracy = model.evaluate(x,y)
+print('Accuracy: %2f' %(accuracy*100))
+
+xnew= loadtxt('tlcsv.csv', delimiter=',')
+#print(xnew)
+pred=xnew[:,0:22]
+
+ynew = model.predict(pred)#using NN to predict outcomes
+first=ynew[0,:]#code had problem wiht only 1 list in the csv to put 2 lists but am only using one for the code
+
+indx= np.argmax(first)#gets max number in array and prints out index 
+tl=indx+1#triage level
+
+for i in range(len(pred)):
+    print("Precantage of each TL=%s" % (ynew[i]))
+
+
+print("The triage level is %s" %(tl))
+
---
+++ path.py
@@ -0,0 +1,10 @@
+import numpy as np
+import csv
+with open('tlcsv.csv', 'w', newline='') as f:
+    thewriter = csv.writer(f)
+
+
+    thewriter.writerow([1,46,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,100.7,76.1,17])
+    thewriter.writerow([1,46,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,100.7,76.1,17])
+    
+  
